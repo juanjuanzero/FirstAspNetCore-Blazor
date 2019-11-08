bcc416f05e3aed83b366277800e275c7c99c1431
Juan E Afable <juanjuanzerojuan@gmail.com>
2019-11-07 21:03:58 -0500

    now able to bring two of the same models into the page. blazor and mvc working together.

--- ChaseRacer/Data/UriHelperImplementation.cs
+++ ChaseRacer/Data/UriHelperImplementation.cs
@@ -6,31 +6,9 @@ using System.Threading.Tasks;
 
 namespace ChaseRacer.Data
 {
-    public class UriHelperImplementation : IUriHelper
+    public class UriHelperImplementation : UriHelperBase
     {
-        public event EventHandler<string> OnLocationChanged;
-
-        public string GetAbsoluteUri()
-        {
-            throw new NotImplementedException();
-        }
-
-        public string GetBaseUri()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void NavigateTo(string uri)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Uri ToAbsoluteUri(string href)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string ToBaseRelativePath(string baseUri, string locationAbsolute)
+        protected override void NavigateToCore(string uri)
         {
             throw new NotImplementedException();
         }
